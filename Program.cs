fcaf503c04954d8702dc8d18b0530d75c2a2f8b3
amfilippov <amfilippov@gmail.com>
2018-04-15 15:42:06 +0300

    еще что то

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+ 
+namespace HelloApp
+{
+    class Program
+    {
+        private static void Main(string[] arg)
+        {
+            Console.WriteLine(DateTime.Now);
+			Console.ReadLine();
+        }
+    }
+}
